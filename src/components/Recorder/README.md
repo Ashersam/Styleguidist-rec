885bf78a73242f6458f58ef295bec9ef2c97568b
Ashersam <ashersamuel06@gmail.com>
2019-11-13 12:15:03 +0530

    init

---
+++ src/components/Recorder/README.md
@@ -0,0 +1,5 @@
+#### Recorder
+```js
+
+<Recorder toggleOpen={true} record={false} />
+```
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Recorder } from './Recorder';
+// export {default as HelloNamesUl} from './hello-names-ul';
+// export {default as HelloNamesList} from './hello-names-list';
